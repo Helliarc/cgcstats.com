045d17a697e8e626cf90082c4cc726ed82925d01
Helliarc <Helliarc@hotmail.com>
2022-10-22 08:28:30 +0000

    Updated functions
    resetFarm and UpdateForms functions were not present in site specific JS

--- js/FarmingSimulator/init.js
+++ js/FarmingSimulator/init.js
@@ -59,3 +59,50 @@ function populateForms(){
         getFarmInit();
     }
 }
+
+/*
+Function limits the NFTs selectable based on NFT Season.  Function called when NFT Season dropdown is changed. Also resets the farm slots pending the seasons being "mixed"
+*/
+function updateForms(){
+    var numNFT = nftSelect.options.length;
+    while(numNFT > 0) {numNFT--; nftSelect.options[numNFT] = null;} 
+    if (seasonSelect.value == "Season 1"){
+        resetFarm();
+        for (var i = 0; i < season1NFT.length; i++){
+            var optn = season1NFT[i].name;
+            var el = document.createElement("option");
+            el.textContent = optn;
+            el.value = optn;
+            nftSelect.appendChild(el);
+        }
+    }
+
+    if (seasonSelect.value == "Season 2"){
+        resetFarm();
+        for (var i = 0; i < season2NFT.length; i++){
+            var optn = season2NFT[i].name;
+            var el = document.createElement("option");
+            el.textContent = optn;
+            el.value = optn;
+            nftSelect.appendChild(el);
+        }
+    }
+}
+
+/*
+Function resets the farming table.  Clears all data and reverts to initial state.
+*/
+function resetFarm(){ // ~HA: Any changes to the farmingSlots class management needs to be update here.  Need to finalize farmingSlots class, or find better way to reset farm.
+    for (let i = 0; i < farmingSlots.length; i++){
+        farmingSlots[i].nft = null;
+        farmingSlots[i].nya[0] = null;
+        farmingSlots[i].image[0] = "img/blank.jpg";
+        farmingSlots[i].fp[0] = null;
+
+        farmingSlots[i].updateImage();
+        farmingSlots[i].updateNYA();
+        farmingSlots[i].updateFP();
+
+        document.getElementById("totalFP").innerHTML = 0;
+    }
+}
